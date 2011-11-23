bd0f137036b92c5c3e509585f0a1ab9161ddbd27
Frederik Roden Lysgaard <frederik.lysgaard@gmail.com>
2011-11-23 14:25:36 +0100

    Our main methods class

---
+++ Dominion/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Dominion
+{
+    using System;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
